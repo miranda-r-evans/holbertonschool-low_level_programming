19be876f4cfd785ecb274f55cd00a6aa1b585a7c
mirandarevans <miranda.r.evans@gmail.com>
2018-02-07 23:34:05 +0000

    checking 100-wildcmp.c

--- 0x07-recursion/100-wildcmp.c
+++ 0x07-recursion/100-wildcmp.c
@@ -1,5 +1,12 @@
 #include "holberton.h"
-#include <stdio.h>
+
+/**
+ * iterate_thru_star - iterates through s2 until it gets to a
+ * non-asterisk value
+ * @s2: the string with an asterisk(s) in it
+ *
+ * Return: a pointer to the next non-asterisk value
+ */
 char *iterate_thru_star(char *s2)
 {
 	s2++;
@@ -10,6 +17,13 @@ char *iterate_thru_star(char *s2)
 	return (s2);
 }
 
+/**
+ * iterate_after - iterates through s1 until it finds a match for s2
+ * @s1: the string we are iterating through
+ * @s2: the string we are finding a match for (after an asterisk)
+ *
+ * Return: a pointer to the matching value in s1
+ */
 char *iterate_after(char *s1, char *s2)
 {
 	if (*s1 != '\0' && *s1 != *s2)
@@ -21,9 +35,16 @@ char *iterate_after(char *s1, char *s2)
 	return (s1);
 }
 
+/**
+ * wildcmp - matches two strings, one of which may have wildcard asterisks
+ * @s1: first string, no asterisks
+ * @s2: second string, may have asterisks
+ *
+ * Return: 1 if string is identical, 0 if not
+ */
 int wildcmp(char *s1, char *s2)
 {
-	int i;
+	int i = 0;
 
 	if (*s1 != *s2 && *s2 != '*')
 		return (0);
@@ -32,7 +53,6 @@ int wildcmp(char *s1, char *s2)
 	{
 		s2 = iterate_thru_star(s2);
 		s1 = iterate_after(s1, s2);
-/*		printf("s1 = %c, s2 = %c\n", *s1, *s2);*/
 	}
 
 	if (*s1 == '\0' && *s2 == '\0')
@@ -41,6 +61,9 @@ int wildcmp(char *s1, char *s2)
 		return (0);
 
 	i = wildcmp(s1 + 1, s2 + 1);
+
+	if (i == 0)
+		i = wildcmp(s1 + 2, s2 - 1);
 
 	return (i);
 }
