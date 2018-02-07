5c2fa8de25abd917d1c602af19bbf2ec623f35c1
mirandarevans <miranda.r.evans@gmail.com>
2018-02-07 20:37:34 +0000

    solved 5-square-root

--- 0x07-recursion/5-sqrt_recursion.c
+++ 0x07-recursion/5-sqrt_recursion.c
@@ -1,16 +1,23 @@
 #include "holberton.h"
-#include <stdio.h>
 
+/**
+ * stop - find the value of input from main minus the square root of input
+ * @stop_value: a value to be checked for the desired stopping point
+ * @i: incremented value used to find stopping point
+ *
+ * Return: 1 if stopping point is found, -1 if stop_value is not the value we
+ * want to stop at
+ */
 int stop(int stop_value, int i)
 {
 	if (i * (i - 1) < stop_value)
 		i = stop(stop_value, i + 1);
 
 	if (i == 1)
-                return (1);
+		return (1);
 
 	if (i * (i - 1) == stop_value)
-                return (1);
+		return (1);
 
 	if (i * (i - 1) > stop_value)
 		return (-1);
@@ -18,7 +25,14 @@ int stop(int stop_value, int i)
 	return (-1);
 }
 
-int _sqrt_recursion(int n)
+/**
+ * find_sr - finds the square root of input from main, or the next lowest
+ * square root if input does not have a natural square root
+ * @n: input number to be evaluated
+ *
+ * Return: square root
+ */
+int find_sr(int n)
 {
 	int i;
 	int j = 1;
@@ -31,12 +45,27 @@ int _sqrt_recursion(int n)
 	i = stop(n, 2);
 
 	if (i == 1)
-	{
-		printf("stop_value = %i\n", n);
 		return (0);
-	}
 
-	j += _sqrt_recursion(n - 1);
+	j += find_sr(n - 1);
 
 	return (j);
+}
+
+/**
+ * _sqrt_recursion - finds the square root of a number
+ * @n: number we find the square root of
+ *
+ * Return: square root, or -1 if no natural square root
+ */
+int _sqrt_recursion(int n)
+{
+	int i = find_sr(n);
+
+	if (i > 0 && i * i == n)
+		return (i);
+	else if (i > 0 && i * i != n)
+		return (-1);
+	else
+		return (i);
 }
