d6498f7161441e5f9d3f6f39ed1ba476fc67436d
mirandarevans <miranda.r.evans@gmail.com>
2018-02-08 05:03:28 +0000

    rewrote 6-is_prime_number.c with better square root function

--- 0x07-recursion/6-is_prime_number.c
+++ 0x07-recursion/6-is_prime_number.c
@@ -1,55 +1,45 @@
 #include "holberton.h"
 
 /**
- * stop - find the value of input from main minus the square root of input
- * @stop_value: a value to be checked for the desired stopping point
- * @i: incremented value used to find stopping point
+ * count_to_sr - iterates from i to the square root of n
+ * @n: number we find the square root of
+ * @i: iterator to check for square root
  *
- * Return: 1 if stopping point is found, -1 if stop_value is not the value we
- * want to stop at
+ * Return: i if square root, -1 if no natural square root
  */
-int stop(int stop_value, int i)
+int count_to_sr(int n, int i)
 {
-	if (i * (i - 1) < stop_value)
-		i = stop(stop_value, i + 1);
+	if (i * i == n)
+		return (i);
 
-	if (i == 1)
-		return (1);
+	if (i * i < n)
+		return (count_to_sr(n, i + 1));
 
-	if (i * (i - 1) == stop_value)
-		return (1);
-
-	if (i * (i - 1) > stop_value)
+	if (i * i > n)
 		return (-1);
 
-	return (-1);
+	return (i);
 }
 
 /**
- * find_sr - finds the square root of input from main, or the next lowest
- * square root if input does not have a natural square root
- * @n: input number to be evaluated
+ * _sqrt_recursion - checks for edge cases and calls function to find
+ * square root
+ * @n: number we find the square root of
  *
- * Return: square root
+ * Return: the square root, or -1 if no natural square root
  */
-int find_sr(int n)
+int _sqrt_recursion(int n)
 {
-	int i;
-	int j = 1;
+	if (n == 1)
+		return (1);
 
 	if (n == 0)
 		return (0);
+
 	if (n < 0)
 		return (-1);
 
-	i = stop(n, 2);
-
-	if (i == 1)
-		return (0);
-
-	j += find_sr(n - 1);
-
-	return (j);
+	return (count_to_sr(n, 2));
 }
 
 /**
@@ -87,13 +77,14 @@ int iterate_to_sr(int n, int sr, int i)
  */
 int is_prime_number(int n)
 {
-	int sr = find_sr(n);
+	int sr = _sqrt_recursion(n);
 
 	if (n < 0)
 		return (0);
 
 	if (sr * sr == n)
 		return (0);
+
 	if (sr * sr != n)
 		sr++;
 
