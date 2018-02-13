aa850f7554bc17c8bd4bc989944241093a2d23a3
mirandarevans <miranda.r.evans@gmail.com>
2018-02-13 22:18:01 +0000

    checking 2-str_concat.c 2

--- 0x0A-malloc_free/2-str_concat.c
+++ 0x0A-malloc_free/2-str_concat.c
@@ -7,6 +7,8 @@
  * @s1: first string
  * @s2: second string
  *
+ * I apologize for the lack of spacing - holberton checker reqs
+ *
  * Return: a pointer to the new string, or NULL if failure
  */
 char *str_concat(char *s1, char *s2)
@@ -18,17 +20,21 @@ char *str_concat(char *s1, char *s2)
 	char *s2_cpy = s2;
 
 	if (s1 == NULL)
+	{
+		s1_cpy = malloc(sizeof(char));
 		*s1_cpy = '\0';
+	}
 	if (s2 == NULL)
+	{
+		s2_cpy = malloc(sizeof(char));
 		*s2_cpy = '\0';
+	}
 	array_cpy = s1_cpy;
 	for (; *array_cpy != '\0'; len++)
 		array_cpy++;
-
 	array_cpy = s2_cpy;
 	for (; *array_cpy != '\0'; len++)
 		array_cpy++;
-
 	array = malloc(sizeof(char) * (len + 1));
 	if (array == NULL)
 		return (NULL);
