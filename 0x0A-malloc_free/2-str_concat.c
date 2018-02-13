bc257bc67678f4903fc1ccc72897266ce59bb373
mirandarevans <miranda.r.evans@gmail.com>
2018-02-13 22:05:37 +0000

    checking 2-str_concat.c

--- 0x0A-malloc_free/2-str_concat.c
+++ 0x0A-malloc_free/2-str_concat.c
@@ -14,44 +14,37 @@ char *str_concat(char *s1, char *s2)
 	char *array = NULL;
 	char *array_cpy;
 	int len = 0;
+	char *s1_cpy = s1;
+	char *s2_cpy = s2;
 
 	if (s1 == NULL)
-		*s1 = '\0';
-
+		*s1_cpy = '\0';
 	if (s2 == NULL)
-		*s2 = '\0';
+		*s2_cpy = '\0';
+	array_cpy = s1_cpy;
+	for (; *array_cpy != '\0'; len++)
+		array_cpy++;
 
-	array_cpy = s1;
+	array_cpy = s2_cpy;
 	for (; *array_cpy != '\0'; len++)
-	{
 		array_cpy++;
-	}
-	array_cpy = s2;
-	for (; *array_cpy != '\0'; len++)
-	{
-		array_cpy++;
-	}
 
 	array = malloc(sizeof(char) * (len + 1));
 	if (array == NULL)
 		return (NULL);
 	array_cpy = array;
-
-	while (*s1 != '\0')
+	while (*s1_cpy != '\0')
 	{
-		*array_cpy = *s1;
+		*array_cpy = *s1_cpy;
 		array_cpy++;
-		s1++;
+		s1_cpy++;
 	}
-
-	while (*s2 != '\0')
+	while (*s2_cpy != '\0')
 	{
-		*array_cpy = *s2;
+		*array_cpy = *s2_cpy;
 		array_cpy++;
-		s2++;
+		s2_cpy++;
 	}
-
 	*array_cpy = '\0';
-
 	return (array);
 }
