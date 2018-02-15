10acc643c52d3f45b9de42db83b03eaf84d609d9
mirandarevans <miranda.r.evans@gmail.com>
2018-02-15 21:48:07 +0000

    finished 101-mul.c style

--- 0x0B-more_malloc_free/101-mul.c
+++ 0x0B-more_malloc_free/101-mul.c
@@ -33,49 +33,24 @@ char *create_array(unsigned int size, char c)
 	return (array);
 }
 
-int main(int ac, char **av)
+/**
+ * mult - multiplies two number strings
+ * @prd: where the product is stored
+ * @tmp: used for calculating product
+ * @len1: length of first factor
+ * @len2: length of second factor
+ * @av: command line input
+ *
+ * Return: pointer to character array containing product
+ */
+char *mult(char *prd, char *tmp, long long int len1, long long len2, char **av)
 {
-	long long int len1;
-	long long int len2;
-	long long int i;
-	long long int j;
-	long long int k;
-	long long int l = 0;
-	int digit;
-	int d1;
-	int d2;
-	int carry = 0;
-	char *temp;
-	char *sum;
+	long long int i, j, k, l = 0;
+	int digit, d1, d2, carry = 0;
 
-	if (ac != 3)
+	for (i = len1 - 1; i >= 0; i--)
 	{
-		printf("Error\n");
-		exit (98);
-	}
-
-	for (len1 = 0; av[1][len1] != '\0'; len1++)
-		if (av[1][len1] < '0' || av[1][len1] > '9')
-		{
-			printf("Error\n");
-			exit (98);
-		}
-
-	for (len2 = 0; av[2][len2] != '\0'; len2++)
-		if (av[2][len2] < '0' || av[2][len2] > '9')
-		{
-			printf("Error\n");
-			exit (98);
-		}
-
-	temp = create_array(len1 + len2 + 1, '0');
-
-	sum = create_array(len1 + len2 + 1, '0');
-
-	for (i = len1 -1; i >= 0; i--)
-	{
-		k = len1 + len2 -1;
-
+		k = len1 + len2 - 1;
 		for (j = len2 - 1; j >= -1; j--)
 		{
 			d1 = av[1][i] - '0';
@@ -91,13 +66,12 @@ int main(int ac, char **av)
 			}
 			else
 				carry = 0;
-			temp[k] = digit + '0';
+			tmp[k] = digit + '0';
 			k--;
 		}
-
 		for (j = len2 + len1 - 1; j >= 0; j--)
 		{
-			digit = temp[j] - '0' + sum[j - l] - '0' + carry;
+			digit = tmp[j] - '0' + prd[j - l] - '0' + carry;
 			if (digit > 9)
 			{
 				carry = digit / 10;
@@ -105,22 +79,73 @@ int main(int ac, char **av)
 			}
 			else
 				carry = 0;
-
-			sum[j - l] = digit + '0';
+			prd[j - l] = digit + '0';
 		}
-
 		l++;
 	}
 
-	for (i = 0; sum[i] == '0'; i++)
+	return (prd);
+}
+
+/**
+ * print_prod - prints the product of multiplying
+ * @prod: array where product is stored
+ * @len1: length of first factor
+ * @len2: length of second factor
+ */
+void print_prod(char *prod, long long int len1, long long int len2)
+{
+	int i;
+
+	for (i = 0; prod[i] == '0'; i++)
 		;
 	for (; i < len1 + len2; i++)
 	{
-		_putchar(sum[i]);
+		_putchar(prod[i]);
 	}
 	_putchar('\n');
+}
 
-	free(sum);
+/**
+ * main - multiplies two number strings from input
+ * @ac: number of elements in av
+ * @av: inputs from command line, numbs to be multiplied
+ *
+ * Return: 0 (Always Success)
+ */
+int main(int ac, char **av)
+{
+	long long int len1, len2;
+	char *temp = NULL; char *prod = NULL;
+
+	if (ac != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	for (len1 = 0; av[1][len1] != '\0'; len1++)
+		if (av[1][len1] < '0' || av[1][len1] > '9')
+		{
+			printf("Error\n");
+			exit(98);
+		}
+	for (len2 = 0; av[2][len2] != '\0'; len2++)
+		if (av[2][len2] < '0' || av[2][len2] > '9')
+		{
+			printf("Error\n");
+			exit(98);
+		}
+	temp = create_array(len1 + len2 + 1, '0');
+	if (temp == NULL)
+		exit(98);
+	prod = create_array(len1 + len2 + 1, '0');
+	if (prod == NULL)
+		exit(98);
+
+	prod = mult(prod, temp, len1, len2, av);
+
+	print_prod(prod, len1, len2);
+	free(prod);
 	free(temp);
 
 	return (0);
