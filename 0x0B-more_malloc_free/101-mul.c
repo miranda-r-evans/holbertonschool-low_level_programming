f36842642837be1262ecc8d7678b1c84e4a92c01
mirandarevans <miranda.r.evans@gmail.com>
2018-02-15 21:05:46 +0000

    saving 101-mul.c while I fix style

--- 0x0B-more_malloc_free/101-mul.c
+++ 0x0B-more_malloc_free/101-mul.c
@@ -3,15 +3,50 @@
 #include <stdlib.h>
 #include <string.h>
 
+/**
+ * create_array - create an array of size size, initialized with a char c
+ * @size: size of the array
+ * @c: character array is initialized with
+ *
+ * Return: a pointer to the array, or NULL is failure
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *array = NULL;
+	unsigned int i;
+	char *array_cpy;
+
+	if (size == 0)
+		return (NULL);
+
+	array = malloc(sizeof(char) * size);
+	if (array == NULL)
+		return (NULL);
+	array_cpy = array;
+
+	for (i = 0; i < size; i++)
+	{
+		*array_cpy = c;
+		array_cpy++;
+	}
+
+	return (array);
+}
+
 int main(int ac, char **av)
 {
 	long long int len1;
 	long long int len2;
 	long long int i;
-	unsigned int j = 1;
-	unsigned int fac1 = 0;
-	unsigned int fac2 = 0;
-	unsigned int sum = 0;
+	long long int j;
+	long long int k;
+	long long int l = 0;
+	int digit;
+	int d1;
+	int d2;
+	int carry = 0;
+	char *temp;
+	char *sum;
 
 	if (ac != 3)
 	{
@@ -21,48 +56,72 @@ int main(int ac, char **av)
 
 	for (len1 = 0; av[1][len1] != '\0'; len1++)
 		if (av[1][len1] < '0' || av[1][len1] > '9')
+		{
+			printf("Error\n");
 			exit (98);
+		}
 
 	for (len2 = 0; av[2][len2] != '\0'; len2++)
 		if (av[2][len2] < '0' || av[2][len2] > '9')
+		{
+			printf("Error\n");
 			exit (98);
+		}
 
-	if (len1 > len2)
-		j = malloc(len1 / 4);
-	else
-		j = malloc(len2 / 4);
-	if (sizeof(j) != len1 / 4 && sizeof(j) != len2 / 4)
-		exit (98);
+	temp = create_array(len1 + len2 + 1, '0');
 
-	fac1 = malloc(len1 / 4);
-	if (sizeof(fac1) != len1 / 4)
-		exit (98);
+	sum = create_array(len1 + len2 + 1, '0');
 
-	fac2 = malloc(len2 / 4);
-	if (sizeof(fac2) != len2 / 4)
-		exit (98);
-
-	sum = malloc((len1 + len2) / 4);
-	if (sum != (len1 + len2) / 4)
-		exit (98);
-
-	for (i = len1 - 1; i >= 0; i--)
+	for (i = len1 -1; i >= 0; i--)
 	{
-		fac1 += (av[1][i] - '0') * j;
-		j *= 10;
+		k = len1 + len2 -1;
+
+		for (j = len2 - 1; j >= -1; j--)
+		{
+			d1 = av[1][i] - '0';
+			d2 = av[2][j] - '0';
+			if (j >= 0)
+				digit = d1 * d2 + carry;
+			else
+				digit = carry;
+			if (digit > 9)
+			{
+				carry = digit / 10;
+				digit = digit % 10;
+			}
+			else
+				carry = 0;
+			temp[k] = digit + '0';
+			k--;
+		}
+
+		for (j = len2 + len1 - 1; j >= 0; j--)
+		{
+			digit = temp[j] - '0' + sum[j - l] - '0' + carry;
+			if (digit > 9)
+			{
+				carry = digit / 10;
+				digit = digit % 10;
+			}
+			else
+				carry = 0;
+
+			sum[j - l] = digit + '0';
+		}
+
+		l++;
 	}
 
-	j = 1;
-
-	for (i = len2 - 1; i >= 0; i--)
+	for (i = 0; sum[i] == '0'; i++)
+		;
+	for (; i < len1 + len2; i++)
 	{
-		fac2 += (av[2][i] - '0') * j;
-		j *= 10;
+		_putchar(sum[i]);
 	}
+	_putchar('\n');
 
-	sum = fac1 * fac2;
-
-	printf("%d\n", sum);
+	free(sum);
+	free(temp);
 
 	return (0);
 }
