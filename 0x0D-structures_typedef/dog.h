8f8346f5b174de1666ee2d23cbf22fc97d5c6dcc
mirandarevans <miranda.r.evans@gmail.com>
2018-02-19 21:18:11 +0000

    checking first two of 1-init_dog.c again

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -1,7 +1,11 @@
 #ifndef DOG_H
 #define DOG_H
 
-#define dog {struct {char *name; float age; char *owner;};}
+struct dog {
+	char *name;
+	float age;
+	char *owner;
+};
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
