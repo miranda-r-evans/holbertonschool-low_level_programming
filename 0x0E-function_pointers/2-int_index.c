e7de4c60b927fc108d492c9cadfa701ed34c19f9
mirandarevans <miranda.r.evans@gmail.com>
2018-02-22 18:03:49 +0000

    adding null to 1 and 2

--- 0x0E-function_pointers/2-int_index.c
+++ 0x0E-function_pointers/2-int_index.c
@@ -14,16 +14,18 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size == 0)
-		return (-1);
-
-	for (i = 0; i < size; i++)
+	if (array != NULL && size != 0 && cmp != NULL)
 	{
-		if (cmp(*array) == 1)
-			return (i);
+		if (size == 0)
+			return (-1);
 
-		array++;
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(*array) == 1)
+				return (i);
+
+			array++;
+		}
 	}
-
 	return (-1);
 }
