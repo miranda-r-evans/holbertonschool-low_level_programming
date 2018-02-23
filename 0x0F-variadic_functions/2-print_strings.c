ea7340002b2532b7801b5a41d3a1a5b41f0292a5
TeamBlocknessmonsters <teamblocknessmonsters@gmail.com>
2018-02-23 19:01:40 +0000

    saving 2-print_strings.c to use as reference

--- 0x0F-variadic_functions/2-print_strings.c
+++ 0x0F-variadic_functions/2-print_strings.c
@@ -1,7 +1,36 @@
 #include <stdarg.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 void print_strings(const char *separator, const unsigned int n, ...)
 {
+	va_list ap;
+	unsigned int i;
+	char *holder;
+	const char *sep = "";
 
+	if (n == 0)
+		exit(0);
+
+	if (separator != NULL)
+		sep = separator;
+
+	va_start(ap, n);
+
+	for (i = 1; i < n; i++)
+	{
+		holder = va_arg(ap, char *);
+		if (holder != NULL)
+			printf("%s%s", holder, sep);
+		else
+			printf("(nil)%s", sep);
+	}
+
+	holder = va_arg(ap, char *);
+	if (holder != NULL)
+		printf("%s\n", holder);
+	else
+		printf("(nil)\n");
+
+	va_end(ap);
 }
---
+++ 0x0F-variadic_functions/3-print_all.c
@@ -0,0 +1,20 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void print_all(const char * const format, ...)
+{
+	va_list ap;
+	unsigned int i;
+	char *c_hold;
+
+	while (*format != '\0')
+	{
+		switch(*format) {
+		case 'c':
+			c_hold
+		}
+
+		format++;
+	}
+}
