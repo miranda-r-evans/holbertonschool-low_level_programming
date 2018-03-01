634b392080ff210533c6cda4f098a1c11f40514d
TeamBlocknessmonsters <teamblocknessmonsters@gmail.com>
2018-03-01 21:13:25 +0000

    adding readme for checker

--- 0x11-singly_linked_lists/100-first.c
+++ 0x11-singly_linked_lists/100-first.c
@@ -2,7 +2,11 @@
 
 void print_string(void) __attribute__((constructor));
 
+/**
+ * print_string - prints a string (before the main function)
+ */
 void print_string(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
---
+++ 0x11-singly_linked_lists/README.md
@@ -0,0 +1,3 @@
+najksd
+dfsnjk
+nfjdsk
