9e9c5d8780a9057a2d7d2c7d75595933e61140d8
Miranda Evans <miranda.r.evans@gmail.com>
2018-03-06 18:11:28 +0000

    forgot to delete something on 9

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,7 +11,7 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *new;
-	listint_t *ptr = *head;
+	listint_t *ptr;
 	unsigned int i = 1;
 
 	if (head == NULL || *head == NULL)
