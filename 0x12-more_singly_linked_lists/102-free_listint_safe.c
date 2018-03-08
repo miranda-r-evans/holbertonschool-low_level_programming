815b34e2b3ae00e20342aa813cf5fb8f4aa984b0
Miranda Evans <miranda.r.evans@gmail.com>
2018-03-08 05:37:15 +0000

    checking 102-free_listint_safe.c

--- 0x12-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x12-more_singly_linked_lists/102-free_listint_safe.c
@@ -10,18 +10,31 @@ size_t free_listint_safe(listint_t **h)
 {
 	size_t elmnts = 0;
 	listint_t *temp = NULL;
+	listint_t *ptr_array[200];
+	int i;
 
-	if (h == NULL || *h == NULL)
+	if (h == NULL)
 		return (0);
 
 	while (*h != NULL)
 	{
+		for (i = 0; i < (int)elmnts; i++)
+		{
+			if (*h == ptr_array[i])
+			{
+				*h = NULL;
+				h = NULL;
+				return (elmnts);
+			}
+		}
+		ptr_array[i] = *h;
 		temp = (*h)->next;
 		free(*h);
 		*h = temp;
 		elmnts++;
 	}
 
+	*h = NULL;
 	h = NULL;
 
 	return (elmnts);
