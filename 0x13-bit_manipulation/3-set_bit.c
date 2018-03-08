574933e7b40adeae1eac726fce8de1c7a0697cab
Miranda Evans <miranda.r.evans@gmail.com>
2018-03-08 22:04:17 +0000

    revised 3-set_bit.c

--- 0x13-bit_manipulation/3-set_bit.c
+++ 0x13-bit_manipulation/3-set_bit.c
@@ -10,36 +10,25 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned int i = 0;
-	unsigned long int cpy = *n;
 	unsigned long int count = 1;
-	unsigned long int remain = 0;
+	unsigned int i = 0;
 
-	while (count * 2 <= cpy || i <= index)
+	while (count * 2 <= *n || i <= index)
 	{
 		i++;
 		count *= 2;
 	}
 
-	if (cpy > count)
-		remain = cpy - count;
-
 	while (i >= index)
 	{
 		if (i == index)
 		{
-			if (cpy == count + remain || count <= remain)
+			if ((count & *n) > 0)
 				return (1);
 
 			*n += count;
 			return (1);
 		}
-		if (count <= remain)
-		{
-			cpy = remain;
-			remain = cpy - count;
-		}
-
 		i--;
 		count /= 2;
 	}
