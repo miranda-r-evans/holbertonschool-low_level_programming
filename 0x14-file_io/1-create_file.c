8914d66c122d8b09aef16a8cd7bf6f320f37dcb2
mirandarevans <miranda.r.evans@gmail.com>
2018-03-13 23:07:18 +0000

    adjusted strlen call in 1

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -45,7 +45,7 @@ int create_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 		return (1);
 
-	len = _strlen(text_content) + 1;
+	len = _strlen(text_content);
 	if (write(fd, text_content, len) < 0)
 		return (-1);
 
