4b9c70baaaf35daddecb0d984a3e85160cd76f8e
mirandarevans <miranda.r.evans@gmail.com>
2018-03-13 23:12:54 +0000

    changed checks for failure on 1

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -34,10 +34,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	if (access(filename, F_OK) != -1)
-		fd = open(filename, O_RDWR | O_TRUNC);
-	else
-		fd = open(filename, O_RDWR | O_CREAT, 0600);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 
 	if (fd < 0)
 		return (-1);
@@ -46,7 +43,7 @@ int create_file(const char *filename, char *text_content)
 		return (1);
 
 	len = _strlen(text_content);
-	if (write(fd, text_content, len) < 0)
+	if (write(fd, text_content, len) < len)
 		return (-1);
 
 	close(fd);
