88b5a839d9dd5f798034d8ee009b61ff5073ee8c
mirandarevans <miranda.r.evans@gmail.com>
2018-06-16 19:23:35 -0700

    corrections to 3, 4, and 5

--- 0x1B-makefiles/5-island_perimeter.py
+++ 0x1B-makefiles/5-island_perimeter.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 '''
 function computes perimeter of island
 input is grid of 1's and 0's
@@ -16,25 +16,12 @@ def island_perimeter(grid):
         for i in range(len(grid)):
                 for j in range(len(grid[0])):
                         if grid[i][j] == 1:
-                                try:
-                                        if grid[i][j-1] == 0:
-                                                perimeter += 1
-                                except IndexError:
+                                if j - 1 < 0 or grid[i][j-1] == 0:
                                         perimeter += 1
-                                try:
-                                        if grid[i][j+1] == 0:
-                                                perimeter += 1
-                                except IndexError:
+                                if j + 1 >= len(grid[0]) or grid[i][j+1] == 0:
                                         perimeter += 1
-                                try:
-                                        if grid[i-1][j] == 0:
-                                                perimeter += 1
-                                except IndexError:
+                                if i - 1 < 0 or grid[i-1][j] == 0:
                                         perimeter += 1
-                                try:
-                                        if grid[i+1][j] == 0:
-                                                perimeter += 1
-                                except IndexError:
+                                if i + 1 >= len(grid) or grid[i+1][j] == 0:
                                         perimeter += 1
-
         return perimeter
