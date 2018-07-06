df29ce6dbf328d9859f3c87e784032916ea9dbd1
mirandarevans <miranda.r.evans@gmail.com>
2018-07-06 09:32:27 -0700

    fixing edge case on 2

--- 0x1C-binary_trees/2-binary_tree_insert_right.c
+++ 0x1C-binary_trees/2-binary_tree_insert_right.c
@@ -13,7 +13,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 
 	if (parent == NULL)
 	{
-		return (new_node);
+		return (NULL);
 	}
 
 	new_node = binary_tree_node(parent, value);
